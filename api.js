73792c8fac68dd61574e107a9a1e18099767c1b0
Luan Silva de Picoli <luansilva_2@hotmail.com>
2025-10-25 16:57:35 -0300

    Corrige Swagger para deploy na Vercel

--- api.js
+++ api.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({ origin: "*" }));
 
 
-app.use('/public', express.static(`${__dirname}/public`));
+app.use('/public', express.static('public', { fallthrough: true }));
 
 
 const swaggerOptions = {
